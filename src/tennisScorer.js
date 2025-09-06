ab8dda00ef8f4b464c76b9f53291e64e6ab6318a
Oliver <oliver.laura@ucb.edu.bo>
2025-09-07 02:20:07 +0900

    feat: test Jugador A marca 1 punto 15-0

--- src/tennisScorer.js
+++ src/tennisScorer.js
@@ -9,6 +9,11 @@
     if(this.judadorA===0 && this.judadorB===0){
       return "love - love";
     } 
+    if(this.judadorA===1 && this.judadorB===0){
+      return "15 - love";
+    } 
+   
+
 }     
   playerA(){
     this.judadorA++;
--- src/tennisScorer.spec.js
+++ src/tennisScorer.spec.js
@@ -4,3 +4,8 @@ test("Marcador inicial  'love-love'",()=>{
     const tennisScorer = new TennisScorer();
     expect(tennisScorer.obtenerPuntuacion()).toBe("love - love");
 });
+test("Marcador 15 - love",()=>{
+    const tennisScorer = new TennisScorer();
+    tennisScorer.playerA();
+    expect(tennisScorer.obtenerPuntuacion()).toBe("15 - love");
+});
