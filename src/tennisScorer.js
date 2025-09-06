a63effd9a4fd76e7a902b8d73ad945eec4e0c665
Oliver <oliver.laura@ucb.edu.bo>
2025-09-07 02:15:20 +0900

    feat: marcador inicial test love-love

--- src/tennisScorer.js
+++ src/tennisScorer.js
@@ -1,19 +1,22 @@
-class TennisScorer {
-  constructor() {
-    this.player1Points = 0;
+ 
+ class TennisScorer {
+ constructor(){
+  this.judadorA=0;
+  this.judadorB=0;
+ }
+
+  obtenerPuntuacion(){
+    if(this.judadorA===0 && this.judadorB===0){
+      return "love - love";
+    } 
+}     
+  playerA(){
+    this.judadorA++;
   }
-  showScore() {
-    if (this.player1Points === 1) {
-      return "15 - Love";
-    } else if (this.player1Points === 2) {
-      return "30 - Love";
-    } else if (this.player1Points === 3) {
-      return "40 - Love";
-    }
-    return "Love - Love";
-  }
-  player1Scores() {
-    this.player1Points++;
-  }
-}
-export default TennisScorer;
+  playerB(){
+    this.judadorB++;
+  } 
+
+} 
+module.exports = TennisScorer;
+
